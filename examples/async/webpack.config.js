cea286865da56ca2a0caa84726469000c966fa09
Juan B. Rodriguez <jbrodriguez@gmail.com>
2016-01-08 11:35:41 -0500

    - Make adjustement to async example (reduce amount of boilerplate in the toolchain)

--- examples/async/webpack.config.js
+++ examples/async/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	devtool: 'eval-source-map',
+	devtool: 'inline-source-map',
 	entry: [
 		'webpack-hot-middleware/client?reload=true',
 		path.join(__dirname, 'src/main.js')
@@ -21,27 +21,23 @@ module.exports = {
 		}),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin(),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('development')
-		})
+		new webpack.NoErrorsPlugin()
+		// new webpack.DefinePlugin({
+		// 	'process.env.NODE_ENV': JSON.stringify('development')
+		// })
 	],
 	module: {
 		loaders: [{
-			loader: 'babel-loader',
+			loader: 'babel',
 			exclude: /node_modules/,
-			test: /\.js$/,
+			test: /\.jsx?$/,
 			query: {
 				plugins: ['transform-runtime'],
-				presets: ['es2015', 'stage-2', 'react']
+				presets: ['react', 'es2015', 'stage-2']
 			}
 		}, {
 			test: /\.json?$/,
 			loader: 'json'
-		}, {
-			test: /\.css?$/,
-			loaders: [ 'style', 'raw' ],
-			include: __dirname
 		}]
 	}
 }
